91193c593832ccf5c1cd1b34e6e66da7b8d4c816
unknown <Eero@Eero-PC.(none)>
2012-04-24 21:29:13 +0100

    blue phase energy is calculated in its own function

--- qlc3d/src/energy.cpp
+++ qlc3d/src/energy.cpp
@@ -3,7 +3,10 @@
 #include <iostream>
 #include <math.h>
 #include <lc.h>
+#include <geometry.h>
+#include <solutionvector.h>
 #include <energy_calculation_macros.h>
+#include <shapefunction3d.h>
 namespace Energy
 {
 #define SIGN(x)		(x)>=0 ? 1:-1
@@ -77,6 +80,181 @@ void init_shape()
 }// enf void init_shape
 
 }// end namespace Energy
+
+
+
+double calculateBluePhaseEnergy(Geometry& geom,
+                                SolutionVector& q,
+                                SolutionVector& v,
+                                LC& mat_par)
+{
+    /*!
+      Calculates the total energy fusing the 'Blue Phase' formulation
+      */
+    Shape4thOrder shapes;
+
+
+    const double A = mat_par.A;
+    const double B = mat_par.B;
+    const double C = mat_par.C;
+    const double rt6 = sqrt(6.0);
+    const double rt2 = sqrt(2.0);
+    const double deleps = mat_par.eps_par - mat_par.eps_per;
+
+    double q0(0);
+    if (mat_par.p0 != 0.0 )
+    {q0 = -2*PI/mat_par.p0;} // '-' SIGN RESULTS IN LEFT HADED CHEIRALITY FOR p0 > 0
+
+    const double K1 = mat_par.K11;
+
+
+
+
+    const Mesh& t = *geom.t;  // REFERENCE TO TETRAHEDRA
+    const double* p = const_cast<const double*>( geom.getPtrTop() );
+
+    double Ftotal = 0;
+#pragma omp parallel for reduction (+:Ftotal)
+    for (idx it = 0 ; it < t.getnElements() ; it++)// FOR EACH TETRAHEDRON
+    {
+        if ( t.getMaterialNumber(it) != MAT_DOMAIN1 )
+            continue;
+
+
+        // LOCAL CACHED COPY OF CURRENT ELEMENT
+        // LOCAL CACHED COPY OF CURRENT ELEMENT AND ITS COORDINATES
+        idx tt[4] = {t.getNode(it,0), t.getNode(it,1) ,
+                     t.getNode(it,2), t.getNode(it,3) };
+
+        double pp[4][3] = { {p[tt[0]*3] , p[tt[0]*3+1] , p[tt[0]*3+2]} ,
+                            {p[tt[1]*3] , p[tt[1]*3+1] , p[tt[1]*3+2]} ,
+                            {p[tt[2]*3] , p[tt[2]*3+1] , p[tt[2]*3+2]} ,
+                            {p[tt[3]*3] , p[tt[3]*3+1] , p[tt[3]*3+2]} };
+
+        // CALCULATE INVERSE JACOBIAN MATRIX FOR CURRENT ELEMENT
+        double xr(0), xs(0), xt(0), yr(0), ys(0), yt(0), zr(0), zs(0), zt(0);
+        for (int i=0;i<4;++i)
+        {
+            xr+=shapes.sh1r[0][i]*pp[i][0];
+            xs+=shapes.sh1s[0][i]*pp[i][0];
+            xt+=shapes.sh1t[0][i]*pp[i][0];
+
+            yr+=shapes.sh1r[0][i]*pp[i][1];
+            ys+=shapes.sh1s[0][i]*pp[i][1];
+            yt+=shapes.sh1t[0][i]*pp[i][1];
+
+            zr+=shapes.sh1r[0][i]*pp[i][2];
+            zs+=shapes.sh1s[0][i]*pp[i][2];
+            zt+=shapes.sh1t[0][i]*pp[i][2];
+        }//end for i
+
+        double Jdet = t.getDeterminant(it)*1e18;
+        double invJdet = 1e6 / Jdet;
+        Jdet*=1e-18;
+
+        // ACTUAL INVERSE JACOBIAN
+        double Jinv[3][3]={{ (zt*ys-yt*zs)*invJdet ,(xt*zs-zt*xs)*invJdet, (xs*yt-ys*xt)*invJdet}
+                           ,{(yt*zr-zt*yr)*invJdet ,(zt*xr-xt*zr)*invJdet, (xt*yr-yt*xr)*invJdet}
+                           ,{(yr*zs-ys*zr)*invJdet ,(xs*zr-xr*zs)*invJdet, (ys*xr-xs*yr)*invJdet}};
+
+        // CREATE LOCAL SHAPE FUNCTION DERIVATIVES
+        double dSh[4][3];
+        for (int i = 0 ; i < 4 ; i++)
+        {
+            dSh[i][0]=shapes.sh1r[0][i]*Jinv[0][0]      // X-DERIVATIVES
+                    + shapes.sh1s[0][i]*Jinv[1][0]
+                    + shapes.sh1t[0][i]*Jinv[2][0];
+
+            dSh[i][1]=shapes.sh1r[0][i]*Jinv[0][1]      // Y
+                    + shapes.sh1s[0][i]*Jinv[1][1]
+                    + shapes.sh1t[0][i]*Jinv[2][1];
+
+            dSh[i][2]=shapes.sh1r[0][i]*Jinv[0][2]      // Z
+                    + shapes.sh1s[0][i]*Jinv[1][2]
+                    + shapes.sh1t[0][i]*Jinv[2][2];
+        }
+
+        // CREATE CACHED COPIES OF VARIABLES Q AND V
+        const double vars[6*4] = {q.getValue(tt[0],0)  , q.getValue(tt[0],1), q.getValue(tt[0],2), q.getValue(tt[0],3), q.getValue(tt[0],4),
+                                  q.getValue(tt[1],0), q.getValue(tt[1],1), q.getValue(tt[1],2), q.getValue(tt[1],3), q.getValue(tt[1],4),
+                                  q.getValue(tt[2],0), q.getValue(tt[2],1), q.getValue(tt[2],2), q.getValue(tt[2],3), q.getValue(tt[2],4),
+                                  q.getValue(tt[3],0), q.getValue(tt[3],1), q.getValue(tt[3],2), q.getValue(tt[3],3), q.getValue(tt[3],4),
+                                  v.getValue(tt[0])  , v.getValue(tt[1])  , v.getValue(tt[2])  , v.getValue(tt[3]   , 0.0                )
+                                 };
+
+
+        for (idx igp = 0 ; igp < shapes.ngp ; ++igp) // FOR EACH GAUSS POINT
+        {
+            // LOCAL SHAPE FUNCTION
+            const double Sh[4] = {shapes.sh1[igp][0], shapes.sh1[igp][1],
+                                  shapes.sh1[igp][2], shapes.sh1[igp][3]};
+
+            // FUNCTION VARIABLES FOR THIS GAUSS POINT
+            double q1(0), q2(0), q3(0), q4(0), q5(0);
+            double q1x(0), q2x(0), q3x(0), q4x(0), q5x(0);
+            double q1y(0), q2y(0), q3y(0), q4y(0), q5y(0);
+            double q1z(0), q2z(0), q3z(0), q4z(0), q5z(0);
+            double Vx(0),  Vy(0) , Vz(0);
+            for(int i=0;i<4;++i)
+            {
+    #define IND(i,j) 5*(i) + (j)
+                q1+=Sh[i]*vars[IND(i,0)];// OPTIMIZE BY PREFETCHING Q AND V TO LOCAL BUFFER AT START OF FUNCTION
+                q2+=Sh[i]*vars[IND(i,1)];
+                q3+=Sh[i]*vars[IND(i,2)];
+                q4+=Sh[i]*vars[IND(i,3)];
+                q5+=Sh[i]*vars[IND(i,4)];
+
+                q1x+=dSh[i][0]*vars[IND(i,0)];
+                q2x+=dSh[i][0]*vars[IND(i,1)];
+                q3x+=dSh[i][0]*vars[IND(i,2)];
+                q4x+=dSh[i][0]*vars[IND(i,3)];
+                q5x+=dSh[i][0]*vars[IND(i,4)];
+
+                q1y+=dSh[i][1]*vars[IND(i,0)];
+                q2y+=dSh[i][1]*vars[IND(i,1)];
+                q3y+=dSh[i][1]*vars[IND(i,2)];
+                q4y+=dSh[i][1]*vars[IND(i,3)];
+                q5y+=dSh[i][1]*vars[IND(i,4)];
+
+                q1z+=dSh[i][2]*vars[IND(i,0)];
+                q2z+=dSh[i][2]*vars[IND(i,1)];
+                q3z+=dSh[i][2]*vars[IND(i,2)];
+                q4z+=dSh[i][2]*vars[IND(i,3)];
+                q5z+=dSh[i][2]*vars[IND(i,4)];
+                // voltages
+                Vx+=dSh[i][0]*vars[20+i];
+                Vy+=dSh[i][1]*vars[20+i];
+                Vz+=dSh[i][2]*vars[20+i];
+            }
+
+            double R = q1*q1 + q2*q2 + q3*q3 + q4*q4 + q5*q5;
+            double mul= shapes.w[igp] *Jdet;
+
+
+            double Felastic(0);
+            double K2 = K1;
+            ENERGY_ELASTIC_2K(Felastic);
+
+            double Fthermotropic=A*(R)/2.0 +
+                    B*(q5*q5*q1*rt6/4.0 - q1*rt6*q2*q2/2.0
+                       -q3*q3*q1*rt6/2.0 + 3.0/4.0*q5*q5*q2*rt2 + 3.0/2.0*q3*rt2*q5*q4
+                       +q4*q4*q1*rt6/4.0 - 3.0/4.0*q4*q4*q2*rt2 + q1*q1*q1*rt6/6.0)/3.0
+                    +C*(R*R)/4.0;
+
+
+            Ftotal += mul*( Felastic + Fthermotropic );
+
+        }// end for igp
+    }// end for each tetrahedron it
+
+    return Ftotal;
+
+
+}
+
+
+
+
 void CalculateFreeEnergy(FILE* fid, Simu* simu, LC* lc, Geometry* geom, SolutionVector* v, SolutionVector* q )
 {
     using std::cout;
@@ -87,6 +265,12 @@ void CalculateFreeEnergy(FILE* fid, Simu* simu, LC* lc, Geometry* geom, Solution
 
 
     cout << " Calculating free energy..."; fflush(stdout);
+
+    if (lc->PhysicsFormulation == LC::BluePhase )
+    {
+        double Ftot = calculateBluePhaseEnergy(*geom, *q, *v, *lc);
+        cout << "BLUE PHASE ENERGY = " << Ftot << endl;
+    }
 
     // IF FIRST ITERATION, PRINT HEADERS
     if (simu->getCurrentIteration() == 1 )
