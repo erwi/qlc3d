a19819bb68b8063897e7adf75794878ae29aba46
eero <eerowillman@gmail.com>
2024-09-23 21:39:01 +0300

    surface definitions are immutable

--- qlc3d/includes/alignment.h
+++ qlc3d/includes/alignment.h
@@ -7,29 +7,38 @@
 #include <stdio.h>
 #include <math.h>
 #include <globals.h>
+#include <geom/vec3.h>
 
 class Vec3;
 enum AnchoringType {Strong = 0, Weak = 1, Homeotropic = 2,
                      Degenerate = 3, Freeze = 5, Polymerise = 6,
                      ManualNodes = 7, AnchoringTypesCount};
 
+/**The surface class represents a single FIXLC anchoring surface.*/
 class Surface {
-    /*!The surface class represents a single FIXLC anchoring surface.*/
+
 private:
-    AnchoringType Type;
-    double Strength;
-    double K1;
-    double K2;
-    double easyAnglesDegrees[3] = {0, 0, 0};                     // Easy direction angles in degrees
-    double v1[3] = {0, 0, 0};                       // First principal axis of anchoring
-    double v2[3] = {0, 0, 0};                       // Second principal axis of anchoring
-    double e[3] = {0, 0, 0};                        // Easy direction vector
-    bool UsesSurfaceNormal;             // whether to use local surface normal vector or v1 and v2
-    bool isFixed;                       // whether this surface is fixed or not
-    bool overrideVolume;               // whether to override volumes at startup. This is set to true by default
-    void calcV1V2();                    // calculates v1 and v2 values from easy angles
-    unsigned int fixLcNumber = 0;
-    void setAnchoringType(const std::string &atype);
+    const AnchoringType Type;
+    const double Strength;
+    const double K1;
+    const double K2;
+    const double easyAnglesDegrees[3];                     // Easy direction angles in degrees
+    const Vec3 v1 = {0, 0, 0};                       // First principal axis of anchoring
+    const Vec3 v2 = {0, 0, 0};                       // Second principal axis of anchoring
+    const Vec3 e = {0, 0, 0};                        // Easy direction vector
+    const bool usesSurfaceNormal;             // whether to use local surface normal vector or v1 and v2
+    const bool isFixed;                       // whether this surface is fixed or not
+    const bool overrideVolume;               // whether to override volumes at startup. This is set to true by default
+    const unsigned int fixLcNumber = 0;
+
+    /**
+    * Calculates v1 and v2 vectors given tilt and twist angles Rotation matrices are given in
+    * Willman, IEEE Trans. Electron Dev. 54, 10, 2007
+    **/
+    //void calcV1V2();
+    //void setAnchoringType(const std::string &atype);
+    [[nodiscard]] static Vec3 calculateV1(double tiltDegrees, double twistDecrees, double rotDegrees = 0);
+    [[nodiscard]] static Vec3 calculateV2(double tiltDegrees, double twistDegrees, double rotDegrees = 0);
 public:
     static const std::vector<std::string> VALID_ANCHORING_TYPES;
     static const std::string DEFAULT_ANCHORING_TYPE;
@@ -40,16 +49,16 @@ public:
     static const std::vector<double> DEFAULT_ANCHORING_PARAMS;
     static const bool DEFAULT_ANCHORING_OVERRIDE_VOLUME;
     std::vector<double> Params;         // holds optional parameters, but is mostly empty
+    Surface(AnchoringType anchoringType, double strength, double k1, double k2,
+            const double easyAnglesDegrees[3],
+            //const Vec3 &v1, const Vec3 &v2,
+            bool usesSurfaceNormal, bool isFixed, bool overrideVolume, unsigned int fixLcNumber);
 
-    Surface(int fxlcnum, const std::string &type);
-    void setStrength(double str);
-    void setK1(double k1);
-    void setK2(double k2);
-    void setEasyAngles(const std::vector<double> &e);
-    void setv1(double v[3]);
-    void setv2(double v[3]);
-    void setEasyVector(double v[3]);
-    void setEnforce(bool overrideVolume) { this->overrideVolume = overrideVolume; }
+    [[nodiscard]] static Surface ofStrongAnchoring(unsigned int fixLcNumber, double tiltDegrees, double twistDegrees);
+    [[nodiscard]] static Surface ofPlanarDegenerate(unsigned int fixLcNumber, double strength);
+    [[nodiscard]] static Surface ofHomeotropic(unsigned int fixLcNumber);
+    [[nodiscard]] static Surface ofWeakAnchoring(unsigned int fixLcNumber,
+                                               double tiltDegrees, double twistDegrees, double strength, double k1, double k2);
 
   [[nodiscard]] std::string getAnchoringTypeName() const;
   [[nodiscard]] AnchoringType getAnchoringType() const;
@@ -58,20 +67,17 @@ public:
   [[nodiscard]] double getK2() const;
   [[nodiscard]] double getEasyTilt() const;
   [[nodiscard]] double getEasyTwist() const;
-  double *getPtrTov1();
-  double *getPtrTov2();
+  [[nodiscard]] const Vec3& getV1() const { return v1; }
+  [[nodiscard]] const Vec3& getV2() const { return v2; }
+  [[nodiscard]] const Vec3& getEasyVector() const { return e; }
+
   [[nodiscard]] bool getUsesSurfaceNormal() const;
   [[nodiscard]] bool isStrong() const;
   [[nodiscard]] bool getOverrideVolume() const { return overrideVolume; }
 
   [[nodiscard]] std::string toString() const;
   [[nodiscard]] unsigned int getFixLCNumber() const { return fixLcNumber; }
-  void setFixLCNumber(unsigned int fixLcNumber);
   friend class Alignment;
-
-  [[nodiscard]] static Surface* ofStrongAnchoring(unsigned int fixLcNumber, double tiltDegrees, double twistDegrees);
-  [[nodiscard]] static Surface* ofPlanarDegenerate(unsigned int fixLcNumber, double strength);
-  [[nodiscard]] static Surface* ofHomeotropic(unsigned int fixLcNumber);
 };
 
 class Reader; // forward declaration of reader class
@@ -82,32 +88,21 @@ private:
     int n_surfaces; // TODO: why not just return surface.size() ??
     void setnSurfaces(int n);
 public:
-    std::vector<Surface *> surface;
+    std::vector<Surface> surface;
     Alignment();
-    ~Alignment();
 
-    void addSurface(const int fixLcNumber,
-                    const std::string &anchoring,
-                    const double &strength,
-                    const std::vector<double> &easyAnglesDegrees,
-                    const double &k1,
-                    const double &k2,
-                    const std::vector<double> &params,
-                    const bool overrideVolume = true);
-
-    void addSurface(Surface *s);
+    void addSurface(Surface s);
 
     [[nodiscard]] const Surface & getSurface(const idx& i) const; // returns read-only reference to i'th surface
 
     double getStrength(int n);  // get strength of FixLCn
     double getK1(int n);        // get K1 of FixLCn
     double getK2(int n);        // get K2 of FixLCn
-    double *getPtrTov1(int n);  // get pointer to v1 of FixLCn
-    double *getPtrTov2(int n);  // get pointer to v2 of FicLCn
+
     [[nodiscard]] int getnSurfaces() const;
     [[nodiscard]] bool IsStrong(int n) const;       // is surface n strong?
-    bool getUsesSurfaceNormal(int n); // if n uses surface normal instead of v1 and v2 ?
-    bool WeakSurfacesExist();   // checks if weak surfaces are defined
+    [[nodiscard]] bool getUsesSurfaceNormal(int n) const; // if n uses surface normal instead of v1 and v2 ?
+    [[nodiscard]] bool weakSurfacesExist() const;
     [[nodiscard]] bool hasSurface(unsigned int fixLcNumber) const;
 };
 #endif
--- qlc3d/src/alignment.cpp
+++ qlc3d/src/alignment.cpp
@@ -4,6 +4,7 @@
 #include <stringenum.h>
 #include <settings_file_keys.h>
 #include <util/exception.h>
+#include <geom/vec3.h>
 
 const std::vector<std::string> Surface::VALID_ANCHORING_TYPES = {"Strong", "Weak", "Homeotropic",
                                                        "Degenerate", "Freeze", "Polymerise",
@@ -16,109 +17,48 @@ const bool Surface::DEFAULT_ANCHORING_OVERRIDE_VOLUME = true;
 const std::vector<double> Surface::DEFAULT_ANCHORING_EASY = {0,0,0};
 const std::vector<double> Surface::DEFAULT_ANCHORING_PARAMS = {};
 
-Surface::Surface(int fxlcnum, const std::string &type) {
-  fixLcNumber = fxlcnum;
-  Strength = DEFAULT_ANCHORING_STRENGTH;
-  K1 = DEFAULT_ANCHORING_K1;
-  K2 = DEFAULT_ANCHORING_K2;
-  setAnchoringType(type);
-  this->setEasyAngles(DEFAULT_ANCHORING_EASY);
-  UsesSurfaceNormal = false;
-  isFixed = true;
-  overrideVolume = DEFAULT_ANCHORING_OVERRIDE_VOLUME;
+Surface::Surface(AnchoringType anchoringType,
+                 double strength, double k1, double k2,
+                 const double easyAnglesDegrees[3],
+                 //const Vec3 &v1, const Vec3 &v2,
+                 bool usesSurfaceNormal,
+                 bool isFixed,
+                 bool overrideVolume,
+                 unsigned int fixLcNumber)
+    : Type(anchoringType), Strength(strength), K1(k1), K2(k2),
+    easyAnglesDegrees{easyAnglesDegrees[0], easyAnglesDegrees[1], easyAnglesDegrees[2]},
+
+    v1(calculateV1(easyAnglesDegrees[0], easyAnglesDegrees[1], easyAnglesDegrees[2])),
+    v2(calculateV2(easyAnglesDegrees[0], easyAnglesDegrees[1], easyAnglesDegrees[2])),
+    e(v1.cross(v2)),
+    usesSurfaceNormal(usesSurfaceNormal), isFixed(isFixed), overrideVolume(overrideVolume), fixLcNumber(fixLcNumber) {
+
 }
 
-void Surface::setAnchoringType(const std::string &atype) {
-    std::string typeKey = wildcardToNum(SFK_FIXLC_ANCHORING, getFixLCNumber());
-    StringEnum<AnchoringType> validator(typeKey, Surface::VALID_ANCHORING_TYPES);
-    this->Type = validator.getEnumValue(atype);
-    switch (Type) {
-        case Strong :
-            isFixed = true;
-            UsesSurfaceNormal = false;
-            break;
-        case Homeotropic :
-            isFixed = true;
-            UsesSurfaceNormal = true;
-            break;
-        case Weak :
-            isFixed = false;
-            UsesSurfaceNormal = false;
-            break;
-        case Degenerate :
-            isFixed = false;
-            UsesSurfaceNormal = true;
-            break;
-        case Freeze :
-            isFixed = true;
-            UsesSurfaceNormal = false;
-            break;
-        case ManualNodes :
-            isFixed = true;
-            UsesSurfaceNormal = false;
-            break;
-        case Polymerise:
-            isFixed = true;
-            UsesSurfaceNormal = false;
-            break;
-        default:
-            throw std::runtime_error("unhandled anchoring type in " + std::string(__PRETTY_FUNCTION__));
-    }
-}// end setAnchoringType
-void Surface::setStrength(double str) {	Strength = str; }
-void Surface::setK1(double k1) { K1 = k1; }
-void Surface::setK2(double k2) { K2 = k2; }
+Vec3 Surface::calculateV1(double tiltDegrees, double twistDecrees, double rotDegrees) {
+  double a = twistDecrees * PI / 180.0; // twist
+  double b = tiltDegrees * PI / 180.0; // tilt
+  double g = rotDegrees * PI / 180.0; // rotation around
 
-/**
-* set easy direction, tilt, twist & rotation angles in degrees
-*/
-void Surface::setEasyAngles(const std::vector<double> &e) {
-    if (e.size() > 3) {
-        std::string errorMsg = std::string("error in Surface::setEasyAngles, too many easy angles,") +
-            " got " + std::to_string(e.size()) + ", but can only handle up to 3 (tilt, twist, rotation)";
-        throw std::invalid_argument(errorMsg);
-    }
-  easyAnglesDegrees[0] = 0.0; easyAnglesDegrees[1] = 0.0; easyAnglesDegrees[2] = 0.0;
-    for (size_t i = 0; i < e.size() ; i++) {
-      easyAnglesDegrees[i] = e[i];
-    }
-
-    this->calcV1V2(); // calculates primary anchoring axes from easy angles
+  return {
+          sin(a) * sin(g) - cos(a) * sin(b) * cos(g),
+          cos(a)*sin(g) + sin(a)*sin(b)*cos(g),
+          cos(b)*cos(g)
+  };
 }
-void Surface::setv1( double v[3] ) { v1[0] = v[0]; v1[1] = v[1] ; v1[2] = v[2]; }
-void Surface::setv2( double v[3] ) { v2[0] = v[0]; v2[1] = v[1] ; v2[2] = v[2]; }
-void Surface::setEasyVector( double v[3]){	e[0] = v[0]; e[1] = v[1]; e[2] = v[2]; }
 
-void Surface::calcV1V2(){
-    /*! Calculates v1 and v2 vectors given tilt and twist angles
- *  Rotation matrices are given in Willman, IEEE Trans. Electron Dev. 54, 10, 2007 
- * */
+Vec3 Surface::calculateV2(double tiltDegrees, double twistDegrees, double rotDegrees) {
+  double a = twistDegrees * PI / 180.0; // twist
+  double b = tiltDegrees * PI / 180.0; // tilt
+  double g = rotDegrees * PI / 180.0; // rotation around
 
-    if (this->Type != Weak) // vectors are only set for 'Weak' anchoring type (why?)
-        return;
-    double a = easyAnglesDegrees[1] * PI / 180.0; // twist
-    double b = easyAnglesDegrees[0] * PI / 180.0; // tilt
-    double g = easyAnglesDegrees[2] * PI / 180.0; // rotation around
-    double k[3] = {0.0, 0.0, 0.0};
-    double l[3] = {0.0, 0.0, 0.0};
-    // apply rotation matrices
-    k[0] = sin(a)*cos(g) + cos(a)*sin(b)*sin(g);
-    k[1] = cos(a)*cos(g) - sin(a)*sin(b)*sin(g);
-    k[2] = -sin(b)*sin(g);
-
-    l[0] = sin(a)*sin(g) - cos(a)*sin(b)*cos(g);
-    l[1] = cos(a)*sin(g) + sin(a)*sin(b)*cos(g);
-    l[2] = cos(b)*cos(g);
-    // v1 is surface normal = (0,0,1) for flat bottom surface
-    this->setv1( l );
-    this->setv2( k );
-    // calculate easy direction vector e = v1 x v2
-    double e[3] = {0,0,0};
-    e[0] =  v1[1]*v2[2] - v2[1]*v1[2];  // OUT OF BOUNDS!!!!
-    e[1] = -v1[0]*v2[2] + v2[0]*v1[2];
-    e[2] =  v1[0]*v2[1] - v2[0]*v1[1];
-    this->setEasyVector( e );
+  return {
+          sin(a)*cos(g) + cos(a)*sin(b)*sin(g),
+          cos(a)*cos(g) - sin(a)*sin(b)*sin(g),
+          -sin(b)*sin(g),
+  };
 }
+
 
 std::string Surface::getAnchoringTypeName() const {
     return Surface::VALID_ANCHORING_TYPES[this->Type];
@@ -126,10 +66,10 @@ std::string Surface::getAnchoringTypeName() const {
 
 std::string Surface::toString() const {
   return fmt::format("FIXLC{}, Anchoring:{}, Strength:{}, K1:{}, K2:{},"
-                     " EasyAngles:[{},{},{}], v1:[{},{},{}], v2:[{},{},{}]",
+                     " EasyAngles:[{},{},{}], v1:[{}], v2:[{}], e:[{}]",
                      getFixLCNumber(), getAnchoringTypeName(), getStrength(), getK1(), getK2(),
                      easyAnglesDegrees[0], easyAnglesDegrees[1], easyAnglesDegrees[2],
-                     v1[0], v1[1], v1[2], v2[0], v2[1], v2[2]);
+                     v1, v2, e);
 }
 
 AnchoringType Surface::getAnchoringType() const {
@@ -141,51 +81,65 @@ double Surface::getK1() const				{		return K1;}
 double Surface::getK2() const				{		return K2;}
 double Surface::getEasyTilt() const{			return easyAnglesDegrees[0];}
 double Surface::getEasyTwist() const{			return easyAnglesDegrees[1];}
-double* Surface::getPtrTov1(){			return &v1[0];}
-double* Surface::getPtrTov2(){			return &v2[0];}
+
 bool	Surface::getUsesSurfaceNormal() const {
-    return UsesSurfaceNormal;
+    return usesSurfaceNormal;
 }
 bool    Surface::isStrong() const {
     return isFixed;
 }
 
-Surface* Surface::ofStrongAnchoring(unsigned int fixLcNumber_, double tiltDegrees, double twistDegrees) {
+Surface Surface::ofStrongAnchoring(unsigned int fixLcNumber_, double tiltDegrees, double twistDegrees) {
   if (fixLcNumber_ == 0 || fixLcNumber_ > 9) {
     RUNTIME_ERROR(fmt::format("FixLC number must be in range 1 - 9, got {}", fixLcNumber_));
   }
-  auto *s = new Surface((int) fixLcNumber_, "Strong");
-  s->setAnchoringType("Strong");
-  s->setStrength(0);
-  s->setEasyAngles({tiltDegrees, twistDegrees, 0});
-  s->setK1(1.);
-  s->setK2(1.);
-  return s;
+  double easyAnglesDegrees[3] = {tiltDegrees, twistDegrees, 0};
+  return {AnchoringType::Strong,
+          DEFAULT_ANCHORING_STRENGTH, 1, 1,
+          easyAnglesDegrees,
+          false, true, true,
+          fixLcNumber_};
 }
 
-Surface* Surface::ofPlanarDegenerate(unsigned int fixLcNumber, double strength) {
+Surface Surface::ofPlanarDegenerate(unsigned int fixLcNumber, double strength) {
   if (fixLcNumber == 0 || fixLcNumber > 9) {
     RUNTIME_ERROR(fmt::format("FixLC number must be in range 1 - 9, got {}", fixLcNumber));
   }
-  auto *s = new Surface((int) fixLcNumber, "Degenerate");
-  s->setStrength(strength);
-  s->setEasyAngles({0, 0, 0});
-  s->setK1(1.);
-  s->setK2(1.);
-  return s;
+  double easyAnglesDegrees[3] = {0, 0, 0};
+
+  return {AnchoringType::Degenerate,
+          strength, 1, 1,
+          easyAnglesDegrees, // actually calculated for each node from surface normal
+          true, false, true, fixLcNumber};
 }
 
-Surface* Surface::ofHomeotropic(unsigned int fixLcNumber) {
+Surface Surface::ofHomeotropic(unsigned int fixLcNumber) {
   if (fixLcNumber == 0 || fixLcNumber > 9) {
     RUNTIME_ERROR(fmt::format("FixLC number must be in range 1 - 9, got {}", fixLcNumber));
   }
-  auto *s = new Surface((int) fixLcNumber, "Homeotropic");
+  double easyAnglesDegrees[3] = {0, 0, 0};
 
-  s->setStrength(0);
-  s->setEasyAngles({0, 0, 0});
-  s->setK1(1.);
-  s->setK2(1.);
-  return s;
+  return {AnchoringType::Homeotropic,
+          DEFAULT_ANCHORING_STRENGTH, 1, 1,
+          easyAnglesDegrees, // actually calculated for each node from surface normal
+          true,
+          true,
+          true,
+          fixLcNumber};
+}
+
+Surface Surface::ofWeakAnchoring(unsigned int fixLcNumber, double tiltDegrees, double twistDegrees, double strength,
+                                 double k1, double k2) {
+  if (fixLcNumber == 0 || fixLcNumber > 9) {
+    RUNTIME_ERROR(fmt::format("FixLC number must be in range 1 - 9, got {}", fixLcNumber));
+  }
+  double easyAnglesDegrees[3] = {tiltDegrees, twistDegrees, 0};
+  return {AnchoringType::Weak,
+          strength, k1, k2,
+          easyAnglesDegrees,
+          false, false, true, fixLcNumber
+
+  };
 }
 
 //====================================================
@@ -195,51 +149,26 @@ Surface* Surface::ofHomeotropic(unsigned int fixLcNumber) {
 //=====================================================
 
 Alignment::Alignment(){	setnSurfaces(0);}//end constructor
-Alignment::~Alignment(){
-    std::vector<Surface*>::iterator itr;
-    for(itr = surface.begin() ; itr!= surface.end() ; ++itr){
-        delete (*itr);
-    }
-}
 
 void Alignment::setnSurfaces(int n){	n_surfaces = n;}
 
-void Alignment::addSurface(Surface* s){
+void Alignment::addSurface(Surface s){
   // check against adding multiple surfaces with the same FixLC number
-  if (hasSurface(s->getFixLCNumber())) {
-    throw std::invalid_argument("Surface with FixLC number " + std::to_string(s->getFixLCNumber()) + " already exists");
+  if (hasSurface(s.getFixLCNumber())) {
+    throw std::invalid_argument("Surface with FixLC number " + std::to_string(s.getFixLCNumber()) + " already exists");
   }
 
   surface.push_back(s);
   n_surfaces++;
 }
 
-void Alignment::addSurface(const int fixLcNumber,
-                           const std::string &anchoring,
-                           const double &strength,
-                           const std::vector<double> &easyAnglesDegrees,
-                           const double &k1,
-                           const double &k2,
-                           const std::vector<double> &params,
-                           const bool overrideVolume) {
-
-    auto s = new Surface(fixLcNumber, anchoring);
-    s->setStrength(strength);
-    s->setEasyAngles(easyAnglesDegrees);
-    s->setK1(k1);
-    s->setK2(k2);
-    s->setEnforce(overrideVolume);
-    s->Params = params;
-    this->addSurface(s);
-}
-
 const Surface& Alignment::getSurface(const idx &i) const {
-    if (i >= (idx) this->surface.size()) {
+    if (i >= (idx) surface.size()) {
         throw std::invalid_argument("no such surface " + std::to_string(i) +
         " in " + __PRETTY_FUNCTION__ );
     }
 
-    return *this->surface[i];
+    return surface[i];
 }
 
 
@@ -252,26 +181,26 @@ bool Alignment::IsStrong(int i) const {
     return getSurface(i).isStrong();
 }
 
-bool Alignment::WeakSurfacesExist(){
-/*!
-returns true if any non-fixed surfaces have been defined
-*/
-    for(int n = 0 ; n < getnSurfaces() ; n++ )
-        if ( !IsStrong(n) )
-            return true;
-    return false;
+bool Alignment::weakSurfacesExist() const {
+  for (const auto &s : surface) {
+    if (!s.isStrong()) {
+      return true;
+    }
+  }
+  return false;
 }
 
-double Alignment::getStrength(int n) 	{return surface[n-1]->getStrength();}
-double Alignment::getK1(int n)			{return surface[n-1]->getK1();}
-double Alignment::getK2(int n)			{return surface[n-1]->getK2();}
-double* Alignment::getPtrTov1(int n)		{return surface[n-1]->getPtrTov1();}
-double* Alignment::getPtrTov2(int n)		{return surface[n-1]->getPtrTov2();}
-bool Alignment::getUsesSurfaceNormal(int n)  {return surface[n-1]->getUsesSurfaceNormal();}
+double Alignment::getStrength(int n) { return surface[n-1].getStrength(); }
+double Alignment::getK1(int n) { return surface[n-1].getK1(); }
+double Alignment::getK2(int n) { return surface[n-1].getK2(); }
+
+bool Alignment::getUsesSurfaceNormal(int n) const {
+  return surface[n-1].getUsesSurfaceNormal();
+}
 
 bool Alignment::hasSurface(unsigned int fixLcNumber) const {
     for (const auto &s : surface) {
-        if (s->getFixLCNumber() == fixLcNumber) {
+        if (s.getFixLCNumber() == fixLcNumber) {
             return true;
         }
     }
