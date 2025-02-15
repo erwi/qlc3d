556abd2e8d19f9b5666cb6df522de45c3496b9ac
eero <eerowillman@gmail.com>
2025-02-15 17:53:33 +0200

    add test for potential solution with neumann boundaries and quadratic elements

--- tests/testutil/test-util.h
+++ tests/testutil/test-util.h
@@ -14,6 +14,7 @@ namespace TestUtil {
   const std::string RESOURCE_SMALL_CUBE_QUADRATIC_GMSH_MESH = "resources/gmsh-small-cube-quadratic.msh";
   /** 1 x 0.1 x 1 mesh with left/right Neumann boundaries and front/back periodic boundaries. Electrode1/FixLC1 on top, Electrode2/FixLC2 on bottom */
   const std::string RESOURCE_PSEUDO_2D_NEUMANN_GMSH_MESH = "resources/pseudo-2d-neumann.msh";
+  const std::string RESOURCE_PSEUDO_2D_NEUMANN_QUADRATIC_GMSH_MESH = "resources/pseudo-2d-neumann-quadratic.msh";
 
   /** 1 x 1 x 1 mesh with left/right/front/back neumann surfaces. Electrode1/FixLC1 on top, Electrode2/FixLC2 on bottom */
   const std::string RESOURCE_UNIT_CUBE_NEUMANN_GMSH_MESH = "resources/unit-cube-neumann.msh";
