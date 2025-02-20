74ace8190522a4b5fd4ee5666c858fd28ccf672d
eero <eerowillman@gmail.com>
2025-02-20 16:55:55 +0200

    cleanup - mostly delete dead code

--- qlc3d/includes/fe/fe-util.h
+++ qlc3d/includes/fe/fe-util.h
@@ -12,23 +12,6 @@ inline bool containsIndex(const unsigned int* start, const unsigned int* end, co
   return std::find(start, end, value) != end;
 }
 
-inline void reorderBoundaryTetNodes(unsigned int tetNodes[4], const unsigned int triNodes[3]) {
-  // find tetNode not in triNodes
-  unsigned int tetNode =  tetNodes[0];
-  for (int i = 0; i < 4; ++i) {
-    tetNode = tetNodes[i];
-    if (!containsIndex(triNodes, triNodes + 3, tetNode)) {
-      tetNode = tetNodes[i];
-      break;
-    }
-  }
-
-  for (int i = 0; i < 3; i++) {
-    tetNodes[i] = triNodes[i];
-  }
-  tetNodes[3] = tetNode;
-}
-
 inline void reorderQuadraticBoundaryTetNodes(std::vector<unsigned int> &tetNodes,
                                              const std::vector<unsigned int> &triNodes,
                                              const Coordinates &coords) {
